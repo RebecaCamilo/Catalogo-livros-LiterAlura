b2e45f40174b3fd745e67f55c2250ddb0a6f76b6
RebecaCamilo <rebecacamilo3@gmail.com>
2024-05-14 11:58:20 -0300

    feat: curso 2

--- src/main/java/com/literalura/bookcatalog/BookCatalogApplication.java
+++ src/main/java/com/literalura/bookcatalog/BookCatalogApplication.java
@@ -1,17 +1,25 @@
 package com.literalura.bookcatalog;
 
+import com.literalura.bookcatalog.principal.Principal;
+import com.literalura.bookcatalog.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookCatalogApplication implements CommandLineRunner {
+	@Autowired
+	private SerieRepository repositorio;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookCatalogApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) {}
+	public void run(String... args) {
+		Principal principal = new Principal(repositorio);
+		principal.exibeMenu();
+	}
 
 }
