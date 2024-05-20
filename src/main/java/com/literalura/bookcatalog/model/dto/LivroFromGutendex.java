ced8230f8577884a891f1a05bb52713308b5ea3b
RebecaCamilo <rebecacamilo3@gmail.com>
2024-05-20 20:05:43 -0300

    build: add spring web as dependency

--- src/main/java/com/literalura/bookcatalog/model/dto/LivroFromGutendex.java
+++ src/main/java/com/literalura/bookcatalog/model/dto/LivroFromGutendex.java
@@ -1,5 +1,8 @@
 package com.literalura.bookcatalog.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record LivroFromGutendex(@JsonAlias("Title") String titulo,
                                 @JsonAlias("totalSeasons") String autor,
